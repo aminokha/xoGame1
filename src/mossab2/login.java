1f94dd6b4e3d2b0a9a63d10298a073b44bb48a77
aminokha <aminekha5@gmail.com>
2017-07-19 09:40:57 +0100

    h

--- src/mossab2/login.java
+++ src/mossab2/login.java
@@ -21,6 +21,7 @@ public class login extends javax.swing.JFrame {
     }
     
     
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
