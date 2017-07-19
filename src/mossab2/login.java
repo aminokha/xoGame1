636a94b842cd1ed237b7b11e7c94bd81b6411703
aminokha <aminekha5@gmail.com>
2017-07-19 09:17:12 +0100

    setLocationRelativeTo(null)

--- src/mossab2/login.java
+++ src/mossab2/login.java
@@ -16,7 +16,7 @@ public class login extends javax.swing.JFrame {
 
     public login() {
         initComponents();
-        
+        this.setLocationRelativeTo(null);
     }
 
     @SuppressWarnings("unchecked")
