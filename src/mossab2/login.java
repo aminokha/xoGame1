036affb4e67d38e6515972f1e3d5995cd71be18d
aminokha <aminekha5@gmail.com>
2017-07-19 09:39:27 +0100

    Signed-off-by: aminokha <aminekha5@gmail.com>

--- src/mossab2/login.java
+++ src/mossab2/login.java
@@ -19,6 +19,8 @@ public class login extends javax.swing.JFrame {
         initComponents();
         this.setLocationRelativeTo(null);
     }
+    
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
